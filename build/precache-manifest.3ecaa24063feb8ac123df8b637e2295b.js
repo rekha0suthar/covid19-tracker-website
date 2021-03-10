849c708b6b178acc19bac7e7c5af3a4205dc6e24
REKHA SUTHAR <71004640+rekha0suthar@users.noreply.github.com>
2021-03-11 01:02:41 +0530

    Add files via upload

---
+++ build/precache-manifest.3ecaa24063feb8ac123df8b637e2295b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac470bc4eb055d4336a29a69e95677c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d000b0d28f2587f158dd",
+    "url": "/static/css/main.425bf73d.chunk.css"
+  },
+  {
+    "revision": "158d6c270d539d972f7f",
+    "url": "/static/js/2.01dacef7.chunk.js"
+  },
+  {
+    "revision": "2c24acf63e18487b21e343b8335b2718",
+    "url": "/static/js/2.01dacef7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d000b0d28f2587f158dd",
+    "url": "/static/js/main.73d9dd11.chunk.js"
+  },
+  {
+    "revision": "ab0d2a7c9407608bc310",
+    "url": "/static/js/runtime-main.ce5ebe61.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
